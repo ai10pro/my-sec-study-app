0e3cad86b0cce9b6ff3ac71c2a739c7f2a677351
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-06-29 19:40:13 +0900

    :sparkles: layout.tsxのFontAwesomeの使い方を変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/app/_components/Header";
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import AuthProvider from "@/app/_contexts/AuthContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTriangleExclamation } from "@fortawesome/free-solid-svg-icons";
+import NextLink from "next/link";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { useAuth } from "@/app/_hooks/useAuth";
+
+// FontAwesome設定
+config.autoAddCss = false;
 
 export const metadata: Metadata = {
   title: "My-Sec-Study-app",
@@ -15,11 +22,37 @@ type Props = {
 
 const RootLayout: React.FC<Props> = (props) => {
   const { children } = props;
+  const { userProfile } = useAuth();
+
+  // if (!userProfile) {
+  //   return (
+  //     <main>
+  //       <div className="text-2xl font-bold">
+  //         <FontAwesomeIcon icon={faTriangleExclamation} className="mr-1.5" />
+  //         ログインが必要なコンテンツ
+  //       </div>
+  //       <div className="mt-4">
+  //         このコンテンツを利用するためには
+  //         <NextLink
+  //           href={`/login`}
+  //           className="px-1 text-blue-500 hover:underline"
+  //         >
+  //           ログイン
+  //         </NextLink>
+  //         してください。
+  //       </div>
+  //     </main>
+  //   );
+  // }
   return (
     <html lang="ja">
       <body>
-        <Header />
-        <main className="mx-4 mt-2 max-w-3xl md:mx-auto">{props.children}</main>
+        <AuthProvider>
+          <Header />
+          <main className="mx-4 mt-2 max-w-3xl md:mx-auto">
+            {props.children}
+          </main>
+        </AuthProvider>
       </body>
     </html>
   );
