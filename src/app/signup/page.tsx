8bccbc95884f68c690abc4606d6976e742440019
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-06-29 22:13:10 +0900

    :sparkles: サインアップページの作成（動作未実装）

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import React, { useEffect, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { useForm } from "react-hook-form";
+import NextLink from "next/link";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner, faPenNib } from "@fortawesome/free-solid-svg-icons";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginRequest, loginRequestSchema } from "@/app/_types/LoginRequest";
+import { TextInputField } from "@/app/_components/TextInputField";
+import { ErrorMsgField } from "@/app/_components/ErrorMsgField";
+import { Button } from "@/app/_components/Button";
+import { UserProfile } from "../_types/UserProfile";
+
+const Page: React.FC = () => {
+  const c_Name = "name";
+  const c_Email = "email";
+  const c_Password = "password";
+
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+  const [isSignUpCompleted, setIsSignUpCompleted] = useState(false);
+
+  // フォームの処理関連の準備と設定
+  const formMethods = useForm<LoginRequest>({
+    mode: "onChange",
+    resolver: zodResolver(loginRequestSchema),
+  });
+  const fieldErrors = formMethods.formState.errors;
+
+  // ルートエラーメッセージのクリアに関する設定
+  useEffect(() => {
+    const subscription = formMethods.watch((value, { name }) => {
+      if (name === c_Email || name === c_Password) {
+        formMethods.clearErrors("root");
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [formMethods]);
+
+  // ログイン完了後のリダイレクト処理
+  useEffect(() => {
+    if (isSignUpCompleted) {
+      router.replace(`/login?${c_Email}=${formMethods.getValues(c_Email)}`);
+      router.refresh();
+      console.log("サインアップ完了");
+    }
+  }, [formMethods, isSignUpCompleted, router]);
+
+  return (
+    <main>
+      <div className="text-2xl font-bold">
+        <FontAwesomeIcon icon={faPenNib} className="mr-1.5" />
+        Signup
+      </div>
+      <form
+        noValidate
+        onSubmit={() => {
+          // フォームの送信処理
+        }}
+        className={twMerge("mt-4 flex flex-col gap-y-4")}
+      >
+        <div>
+          <label htmlFor={c_Name} className="mb-2 block font-bold">
+            表示名
+          </label>
+          <TextInputField
+            {...formMethods.register(c_Name)}
+            id={c_Name}
+            placeholder="寝屋川 タヌキ"
+            type="text"
+            disabled={isPending || isSignUpCompleted}
+            error={!fieldErrors.name}
+            autoComplete="name"
+          />
+          <ErrorMsgField msg={fieldErrors.name?.message} />
+        </div>
+
+        <div>
+          <label htmlFor={c_Email} className="mb-2 block font-bold">
+            メールアドレス（ログインID）
+          </label>
+          <TextInputField
+            {...formMethods.register(c_Email)}
+            id={c_Email}
+            placeholder="name@example.com"
+            type="email"
+            disabled={isPending || isSignUpCompleted}
+            error={!fieldErrors.email}
+            autoComplete="email"
+          />
+          <ErrorMsgField msg={fieldErrors.email?.message} />
+        </div>
+
+        <div>
+          <label htmlFor={c_Password} className="mb-2 block font-bold">
+            パスワード
+          </label>
+          <TextInputField
+            {...formMethods.register(c_Password)}
+            id={c_Password}
+            placeholder="*****"
+            type="password"
+            disabled={isPending || isSignUpCompleted}
+            error={!fieldErrors.password}
+            autoComplete="off"
+          />
+          <ErrorMsgField msg={fieldErrors.password?.message} />
+          <ErrorMsgField msg={fieldErrors.root?.message} />
+        </div>
+
+        <Button
+          variant="indigo"
+          width="stretch"
+          className="tracking-widest"
+          disabled={
+            !formMethods.formState.isValid ||
+            formMethods.formState.isSubmitting ||
+            isSignUpCompleted
+          }
+        >
+          登録
+        </Button>
+      </form>
+
+      {isSignUpCompleted && (
+        <div>
+          <div className="mt-4 flex items-center gap-x-2">
+            <FontAwesomeIcon icon={faSpinner} spin />
+            <div>サインアップが完了しました。ログインページに移動します。</div>
+          </div>
+          <NextLink
+            href={`/login?${c_Email}=${formMethods.getValues(c_Email)}`}
+            className="text-blue-500 hover:underline"
+          >
+            自動的に画面が切り替わらないときはこちらをクリックしてください。
+          </NextLink>
+        </div>
+      )}
+    </main>
+  );
+};
+
+export default Page;
