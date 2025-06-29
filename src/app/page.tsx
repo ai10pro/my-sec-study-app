dba385e77e7f75b4157e37399ec4e8a39c659f26
ai10pro <106215301+ai10pro@users.noreply.github.com>
2025-06-29 23:23:02 +0900

    :sparkles: メインにリンクの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 "use client";
 
+import NextLink from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+
+const links = [
+  {
+    href: "/login",
+    label: "Login",
+    info: "トークンベース認証",
+  },
+  {
+    href: "/signup",
+    label: "Signup",
+    info: "ユーザー登録",
+  },
+];
+
 const Page: React.FC = () => {
   return (
     <main>
-      <div className="font-bold text-2xl">Main</div>
+      <div className="text-2xl font-bold">Main</div>
+      <div className="mt-4 ml-2 gap-y-2">
+        {links.map(({ href, label, info }) => (
+          <div key={href} className="flex items-center">
+            <FontAwesomeIcon icon={faCode} className="mr-1.5" />
+            <NextLink href={href} className="mr-2 hover:underline">
+              {label}
+            </NextLink>
+            <div className="text-xs text-slate-600">※ {info}</div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
